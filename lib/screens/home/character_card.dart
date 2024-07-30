fdcfc34acac01d5b09107a061bf8bfd61533b6a6
Leith Hobson <33805066+leithhobson@users.noreply.github.com>
2024-07-31 01:25:27 +0200

    Add favourite management to the Home page character list

--- lib/screens/home/character_card.dart
+++ lib/screens/home/character_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/screens/profile/heart.dart';
 import 'package:flutter_rpg/screens/profile/profile.dart';
 import 'package:flutter_rpg/shared/styled_text.dart';
 import 'package:flutter_rpg/theme.dart';
@@ -9,50 +10,65 @@ class CharacterCard extends StatelessWidget {
 
   final Character character;
 
+  void openCharacterProfile(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) => Profile(characterId: character.id)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (ctx) => Profile(characterId: character.id)));
+        openCharacterProfile(context);
       },
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          child: Row(
-            children: [
-              Hero(
-                tag: character.id,
-                child: Image.asset(
-                  'assets/img/vocations/${character.vocation.image}',
-                  width: 50,
-                ),
+      child: Stack(
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: Row(
+                children: [
+                  Hero(
+                    tag: character.id,
+                    child: Image.asset(
+                      'assets/img/vocations/${character.vocation.image}',
+                      width: 50,
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(character.name),
+                        StyledText(character.vocation.title),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      openCharacterProfile(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_forward_ios,
+                      color: AppColours.textColor,
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    StyledHeading(character.name),
-                    StyledText(character.vocation.title),
-                  ],
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.arrow_forward_ios,
-                  color: AppColours.textColor,
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          Positioned(
+            left: -10,
+            top: -10,
+            child: Heart(character: character),
+          ),
+        ],
       ),
     );
   }
